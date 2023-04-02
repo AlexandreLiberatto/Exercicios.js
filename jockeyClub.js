84c299aafa921a3f6d40994cb6f8eb6357209e60
AlexandreLiberatto <118403407+AlexandreLiberatto@users.noreply.github.com>
2023-04-02 20:08:38 -0300

    acrescentando function obterCavalo

--- jockeyClub.js
+++ jockeyClub.js
@@ -3,25 +3,34 @@ const respLista = document.querySelector("pre");
 const respCavalo = document.querySelector("#outCavalo");
 
 // Nome dos cavalos participantes do páreo
-const CAVALOS =["Marujo", "Tordilho", "Belga", "Twister", "Jade", "Lucky"]
+const CAVALOS = ["Marujo", "Tordilho", "Belga", "Twister", "Jade", "Lucky"];
 
 // Vetor que irá armazenar um objeto aposta (com o número do cavalo e valor da aposta)
-const apostas = []
+const apostas = [];
 
 frm.addEventListener("submit", (e) => {
-    e.preventDefault()
+  e.preventDefault();
 
-    const cavalo = Number(frm.inCavalo.value)
-    const valor = Number(frm.inValor.value)
+  const cavalo = Number(frm.inCavalo.value);
+  const valor = Number(frm.inValor.value);
 
-    // Adiciona ao vetor de objetos (atributos cavalo e valor)
-    apostas.push({cavalo, valor})
-    //variável para exibir a lista de apostas realizadas
-    let lista = `Apostas realizadas\n ${"-" .repeat(25)}\n`
+  // Adiciona ao vetor de objetos (atributos cavalo e valor)
+  apostas.push({ cavalo, valor });
+  //variável para exibir a lista de apostas realizadas
+  let lista = `Apostas realizadas\n ${"-".repeat(25)}\n`;
 
-    // Percorre o vetor e concatena em lista as apostas realizadas
-    for (const aposta of apostas) {
-        lista += `Nº ${aposta.cavalo} ${obterCavalo(aposta.cavalo)}`
-        lista += ` - R$: ${aposta.valor.toFixed(2)}\n`
-    }
-})
+  // Percorre o vetor e concatena em lista as apostas realizadas
+  for (const aposta of apostas) {
+    lista += `Nº ${aposta.cavalo} ${obterCavalo(aposta.cavalo)}`;
+    lista += ` - R$: ${aposta.valor.toFixed(2)}\n`;
+  }
+  respLista.innerText = lista; // Exibe a lista das apostas
+
+  frm.reset();
+  frm.inCavalo.focus();
+});
+
+const obterCavalo = (num) => {
+  const posicao = num - 1; // Posição no vetor (subtrai 1, pois inicia em zero)
+  return CAVALOS[posicao]; // nome do cavalo (da const CAVALOS)
+};
